00947ba71e3511bdd9aa1d2087d3e4ac14237d15
abiglin <61331791+abiglin@users.noreply.github.com>
2023-06-09 10:12:24 -0400

    Moving text to the right

--- SIR/sir2.py
+++ SIR/sir2.py
@@ -150,8 +150,8 @@ Extension_button.pack(side=LEFT)
 Extension_button.flash()
 
 canvas1 = Canvas(top)
-canvas1.create_text(10,80,text="System:"+"\n"+"  dS / dt = -(α / N) * S * I"+"\n"+"  dI / dt = (α / N) * S * I - β * I"+"\n"+"  dR / dt = β * I"+"\n"+"\n"+"  α: infection by contact"+"\n"+"  β: death by infection"+"\n"+"  N: total population (=S+I+R)",font=labelFont,activefill='red',justify=LEFT)
-canvas1.create_text(10,185,text="Initial Conditions, Parameters:",font=labelFont,activefill='red',justify=LEFT)
+canvas1.create_text(200,80,text="System:"+"\n"+"  dS / dt = -(α / N) * S * I"+"\n"+"  dI / dt = (α / N) * S * I - β * I"+"\n"+"  dR / dt = β * I"+"\n"+"\n"+"  α: infection by contact"+"\n"+"  β: death by infection"+"\n"+"  N: total population (=S+I+R)",font=labelFont,activefill='red',justify=LEFT)
+canvas1.create_text(200,185,text="Initial Conditions, Parameters:",font=labelFont,activefill='red',justify=LEFT)
 canvas1.pack()
 
 infection_label.place(x=5,y=295)
